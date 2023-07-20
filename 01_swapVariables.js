82f3c454dd87ef532f5de93b7694dd74b9789acd
Aditya-2602 <adibhange2602@gmail.com>
2023-07-20 16:37:13 +0530

    swapped the names & values in JS
    swapped the names & values in JS

---
+++ 01_swapVariables.js
@@ -0,0 +1,41 @@
+
+console.log('Swap two names');
+var sweety = "Sweety";
+var cutie = "Cutie";
+
+console.log('Before swap:', 'sweety:', sweety, 'cutie:',cutie);
+
+var temp = sweety;
+sweety = cutie;
+cutie = temp;
+console.log('After swap:', 'sweety:',sweety,'cutie:',cutie);
+console.log('------------------');
+console.log('Swap three values');
+var num1 = 100;
+var num2 = 200;
+var num3 = 300;
+console.log('Before swap:','num1:',num1,'num2:',num2,'num3:',num3);
+var temp = num1;
+num1 = num2;
+num2 = num3;
+num3 = temp;
+console.log('after swap','num1:',num1,'num2:',num2,'num3:',num3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
